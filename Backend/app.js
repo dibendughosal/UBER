e544050def3d8670ba6cae1758bed66c0ba702e9
Dibendu Ghosal <128916258+dibendughosal@users.noreply.github.com>
2025-03-29 01:55:10 +0530

    Building UBER

--- Backend/app.js
+++ Backend/app.js
@@ -4,6 +4,8 @@ const express = require('express');
 const app = express();
 const cors = require('cors')
 app.use(cors()); 
+const connectToDB = require('./databases/db')
+connectToDB();
 
 app.get("/", (req,res)=>{
     res.send("<h1>Welcome from App.js</h1>")
