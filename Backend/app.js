1166391c8e6b5a5fbd949ea0c68bd779eed291aa
Dibendu Ghosal <128916258+dibendughosal@users.noreply.github.com>
2025-03-29 21:32:52 +0530

    Building UBER

--- Backend/app.js
+++ Backend/app.js
@@ -3,12 +3,19 @@ dotenv.config();
 const express = require('express');
 const app = express();
 const cors = require('cors')
-app.use(cors()); 
 const connectToDB = require('./databases/db')
+const userRoutes = require('./routes/userRoutes');
 connectToDB();
+
+app.use(cors()); 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}))
+
 
 app.get("/", (req,res)=>{
     res.send("<h1>Welcome from App.js</h1>")
 })
+
+app.use('/user', userRoutes)
 
 module.exports = app;
